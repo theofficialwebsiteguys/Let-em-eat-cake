4b1cc522bd26a01bb7681ba400fa8fbf4800df81
theofficialwebsiteguys <theofficialwebsiteguys@gmail.com>
2024-05-09 00:44:25 -0400

    05.09.2024: navbar changes and color changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { ServicesComponent } from './services/services.component';
 import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'services', component: ServicesComponent },
     { path: 'gallery', component: GalleryComponent },
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  constructor(private router: Router) { }
+  //constructor(public router: Router) { }
 
+  constructor(public router: Router, private route: ActivatedRoute) { }
 
-  route(page: string){
-    this.router.navigate(['/' + page]);
-  }
+  // route(page: string) {
+  //   this.router.navigate(['/' + page]);
+
+  // }
 
 }
