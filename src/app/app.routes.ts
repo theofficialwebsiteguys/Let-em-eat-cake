9fdc6b97a55051ba973ca52fd427f6744fc32aaa
Full Name <theofficialwebsiteguys@gmail.com>
2024-05-09 16:42:39 -0400

    home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirects to /home
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'gallery', component: GalleryComponent },
