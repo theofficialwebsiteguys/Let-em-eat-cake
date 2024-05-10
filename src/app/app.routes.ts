46979fdfad5346bbebc7602bfd527776760e4855
Full Name <theofficialwebsiteguys@gmail.com>
2024-05-10 17:55:04 -0400

    Added wedding page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { WeddingsComponent } from './weddings/weddings.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirects to /home
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'weddings', component: WeddingsComponent },
     { path: 'gallery', component: GalleryComponent },
 ];
---
+++ src/app/weddings/weddings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weddings',
+  standalone: true,
+  imports: [],
+  templateUrl: './weddings.component.html',
+  styleUrl: './weddings.component.css'
+})
+export class WeddingsComponent {
+
+}
